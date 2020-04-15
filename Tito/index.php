74124a8d9b823bc5dbb0e7090cd12d2c2e240ccb
kenshi08@me.com <kenshi08@gmail.com>
2020-04-15 21:28:16 +0800

    revert back changes

--- Tito/index.php
+++ Tito/index.php
@@ -97,10 +97,6 @@ $(document).ready(function () {
             <div class="text-vertical-center">
                 <h1>Tito</h1>
                 <h3>Time Traffic Overview - APJ CMBU</h3>
-                <h5>Stay safe!</h5>
-                <h5>Stay safe!</h5>
-                <h5>Stay safe!</h5>
-                <!-- <h5>by Vince :)</h5> -->
                  <?PHP
                  echo "Runs on : " . gethostname() . " (" . getHostByName(getHostName()) .")";
 
