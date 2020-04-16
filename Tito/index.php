04ffac637d88fa84921870f2f587861d32557a95
kenshi08@me.com <kenshi08@gmail.com>
2020-04-16 12:00:27 +0800

    modified text UI

--- Tito/index.php
+++ Tito/index.php
@@ -97,10 +97,10 @@ $(document).ready(function () {
             <div class="text-vertical-center">
                 <h1>Tito</h1>
                 <h3>Time Traffic Overview - APJ CMBU</h3>
+                <h3>Stay Safe Everyone!</h3>
                  <?PHP
                  echo "Runs on : " . gethostname() . " (" . getHostByName(getHostName()) .")";
-
-                                 ?>
+                ?>
                 <br>
                 <br>
                 <a href="#about" class="btn btn-dark btn-lg">Find Out More</a>
